715c7d5300338986eac5650792fe91d4d71fc251
oswin902m <1665434994@qq.com>
2026-05-21 13:21:50 +0800

    db validate with standard schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,6 @@
 import { integer, pgTable, varchar } from 'drizzle-orm/pg-core';
+import { createInsertSchema, createSelectSchema, createUpdateSchema } from 'drizzle-orm/zod';
+import { z } from 'zod';
 
 // https://orm.drizzle.team/docs/column-types/pg
 export const usersTable = pgTable('users', {
@@ -7,3 +9,13 @@ export const usersTable = pgTable('users', {
   age: integer().notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
 });
+
+// https://orm.drizzle.team/docs/zod
+export const userSelectSchema = createSelectSchema(usersTable);
+export const userInsertSchema = createInsertSchema(usersTable);
+export const userUpdateSchema = createUpdateSchema(usersTable);
+
+// typeof usersTable.$inferInsert
+export type UserSelectType = z.infer<typeof userSelectSchema>;
+export type UserInsertType = z.infer<typeof userInsertSchema>;
+export type UserUpdateType = z.infer<typeof userUpdateSchema>;
--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,34 @@
 import { eq } from 'drizzle-orm';
 
-import { usersTable } from './db/schema';
+import {
+  userInsertSchema,
+  UserInsertType,
+  userSelectSchema,
+  usersTable,
+  userUpdateSchema,
+  UserUpdateType,
+} from './db/schema';
 import { db } from './libs/drizzle';
 
 async function main() {
-  const user: typeof usersTable.$inferInsert = {
+  const user: UserInsertType = {
     name: 'John',
     age: 30,
     email: 'john@example.com',
   };
-
-  await db.insert(usersTable).values(user);
+  const parsedUser = userInsertSchema.parse(user);
+  await db.insert(usersTable).values(parsedUser);
   console.log('New user created!');
 
   const users = await db.select().from(usersTable);
-  console.log('Getting all users from the database: ', users);
+  const parsedUsers = userSelectSchema.parse(users);
+  console.log('Getting all users from the database: ', parsedUsers);
 
-  await db
-    .update(usersTable)
-    .set({
-      age: 31,
-    })
-    .where(eq(usersTable.email, user.email));
+  const updatedUser: UserUpdateType = {
+    age: 31,
+  };
+  const parsedUpdatedUser = userUpdateSchema.parse(updatedUser);
+  await db.update(usersTable).set(parsedUpdatedUser).where(eq(usersTable.email, user.email));
   console.log('User info updated!');
 
   // await db.delete(usersTable).where(eq(usersTable.email, user.email));
